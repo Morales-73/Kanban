c648b1a97b3a404c8757d0551bb9daf664838dee
Ciro <ciro.morales591@gmail.com>
2022-09-18 17:58:59 -0300

    Tablero Kanban

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Columna from './components/Columna';
+
+function App() {
+  return (
+    <>
+      <Columna/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Columna.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Columna = () => {
+  return (
+    <>
+          <div className="container text-center">
+              <div class="row">
+                  <div class="col">
+                      Column
+                  </div>
+                  <div class="col">
+                      Column
+                  </div>
+                  <div class="col">
+                      Column
+                  </div>
+                  <div class="col">
+                      Column
+                  </div>
+              </div>
+          </div>
+    </>
+  )
+}
+
+export default Columna
