0ac3a2ad705ce85c5bc70435eda41a2c9c2d58d3
Tadeo Pariani <parianitadeo@gmail.com>
2022-09-18 18:53:15 -0300

    Update Columna.jsx
    margin-4

--- src/components/Columna.jsx
+++ src/components/Columna.jsx
@@ -5,18 +5,18 @@ const Columna = () => {
     <>
           <div className="container text-center p-1">
               <div class="row p-5">
-                  <div class="col border border-primary pl-1 pr-1 m-5">
+                  <div class="col border border-primary pl-1 pr-1 m-4">
                       Column 
                   </div>
-                  <div class="col border border-primary pl-1 pr-1 m-5">
+                  <div class="col border border-primary pl-1 pr-1 m-4">
                       Column
 
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt corporis dolores, recusandae molestias molestiae eligendi pariatur voluptate earum in reiciendis, perspiciatis doloribus accusantium fugit commodi porro sit, dignissimos necessitatibus? Natus.
                   </div>
-                  <div class="col border border-primary pl-1 pr-1 m-5">
+                  <div class="col border border-primary pl-1 pr-1 m-4">
                       Column
                   </div>
-                  <div class="col border border-primary pl-1 pr-1 m-5">
+                  <div class="col border border-primary pl-1 pr-1 m-4">
                       Column
                   </div>
               </div>
