e5ecf9b2ce716228dca12658d279d3c34f351be5
Tadeo Pariani <parianitadeo@gmail.com>
2022-09-18 19:30:22 -0300

    Update Columna.jsx

--- src/components/Columna.jsx
+++ src/components/Columna.jsx
@@ -6,19 +6,33 @@ const Columna = () => {
           <div className="container text-center p-1">
               <div class="row p-5">
                   <div class="col border border-primary pl-1 pr-1 m-4">
-                      Column 
+                      <h2>EEEE</h2> 
+
+                      <div class="border border-primary">
+                        <p>limpiar platos</p>
+                        <p>URGENCIA: 1</p>
+
+                        </div>
                   </div>
+
                   <div class="col border border-primary pl-1 pr-1 m-4">
                       Column
 
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt corporis dolores, recusandae molestias molestiae eligendi pariatur voluptate earum in reiciendis, perspiciatis doloribus accusantium fugit commodi porro sit, dignissimos necessitatibus? Natus.
                   </div>
+
                   <div class="col border border-primary pl-1 pr-1 m-4">
                       Column
+
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt corporis dolores, recusandae molestias molestiae eligendi pariatur voluptate earum in reiciendis, perspiciatis doloribus accusantium fugit commodi porro sit, dignissimos necessitatibus? Natus.
                   </div>
+
                   <div class="col border border-primary pl-1 pr-1 m-4">
                       Column
+
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt corporis dolores, recusandae molestias molestiae eligendi pariatur voluptate earum in reiciendis, perspiciatis doloribus accusantium fugit commodi porro sit, dignissimos necessitatibus? Natus.
                   </div>
+
               </div>
           </div>
     </>
