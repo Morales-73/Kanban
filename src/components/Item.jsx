6416cf25ddce0969d366536698da4b9fa42f8446
Tadeo Pariani <parianitadeo@gmail.com>
2022-09-21 18:06:00 -0300

    actualizado

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import '../index.css'
 import FormEditarItem from './EditarItem'
 
-const Item = ({item, eliminarItem, editarItem}) => {
+const Item = ({item}) => {
 
+
+   
   return (
     <>
         <div>
